9ad6714ee1c39cb1d240efb4783e547c613d9d53
nicolevito <coemakeup@gmail.com>
2025-05-01 09:57:00 -0300

    Adicionando testes

--- product/urls.py
+++ product/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from product.viewsets import ProductViewSet 
+from product.viewsets import ProductViewSet, CategoryViewSet
 
 router = routers.SimpleRouter()
 router.register(r'products', ProductViewSet, basename='product')
+router.register(r'category', CategoryViewSet, basename='category')
 
 urlpatterns = [
     path('', include(router.urls)),
